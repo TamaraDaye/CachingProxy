af0a6220e85ce89c69fa39335456428d54f19354
Tamara <tamaradaye0gmail.com>
2025-11-10 12:25:42 +0100

    Trying to handle request data

--- multiconn-server.py
+++ multiconn-server.py
@@ -1,24 +1,58 @@
 import socket
+from http.server import BaseHTTPRequestHandler
+from io import BytesIO
 import threading
 import argparse
 
 CACHE = {}
 
 
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = BytesIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.parse_request()
+
+
+# FIXME: handle the args properly
 def get_args():
     parser = argparse.ArgumentParser()
-
     subparser = parser.add_subparsers(dest="cmd", required=True)
-
-    caching_proxy = subparser.add_parser("caching_proxy")
-
+    caching_proxy = subparser.add_parser("caching-proxy")
     caching_proxy.add_argument("--port", type=int)
-
-    caching_proxy.add_argument("--host", type=str)
-
+    caching_proxy.add_argument("--origin", type=str)
+    caching_proxy.add_argument("--clear-cache", type=str)
     args = parser.parse_args()
 
-    return args.port, args.host
+    return args
+
+
+# TODO :implment the response from client if cache miss
+def get_response(request):
+    url = get_args().origin
+    port = 80
+    print(url)
+    print(request)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        try:
+            client.connect((url, port))
+            client.sendall(request)
+            buffer = b""
+            while True:
+                chunk = client.recv(1024)
+                if not chunk:
+                    print(f"Client terminated connection {url}")
+                    break
+                buffer += chunk
+            response = buffer
+        except Exception as e:
+            print(f"Exception found {e}")
+        return response
+
+
+# FIXME:Handle request data for forwarding
+def request_handler(data):
+    location = get_args().origin
 
 
 def handle_client(client_socket, addr):
@@ -27,29 +61,45 @@ def handle_client(client_socket, addr):
     with client_socket as client:
         try:
             while True:
-                request = client.recv(1024)
+                chunk = client.recv(1024)
 
-                if not request:
+                if not chunk:
                     print(f"Client {addr[0]} terminated connection")
                     break
 
-                buffer += request
-                CACHE[addr] = buffer
-                print(CACHE)
-                client.send(b"Recieved your request")
+                buffer += chunk
+
+                if b"\r\n\r\n" in buffer:
+                    break
+
+            request_data = buffer
+
+            if request_data in CACHE:
+                client.sendall(CACHE[request_data])
+
+            else:
+                request = parse_request(request_data)
+                print(request)
+                response = get_response(request_data)
+                print(response, request_data)
+                # pprint.pprint(CACHE, indent=4)
+                CACHE[request_data] = response
+                client.sendall(response)
+
         except Exception as e:
             print(f"Caught an exception {e}")
 
 
 def run_server():
-    server_ip = "0.0.0.0"
-    port = 8080
+    args = get_args()
+    PORT = args.port
+    SERVER_IP = "0.0.0.0"
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((server_ip, port))
+        s.bind((SERVER_IP, PORT))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen()
-        print(f"Listening on {server_ip}")
+        print(f"Listening on {PORT}")
 
         while True:
             client_socket, addr = s.accept()
